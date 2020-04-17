ee2639f2b7f08112617b13ecc06a13658cd8091b
Amilcar Serrano <amilcar.sr8@gmail.com>
2020-04-17 04:58:42 -0600

    Added Spanish translations, updated gradle build tools and made a minor code cleanup for a required context

--- app/src/main/java/io/bluetrace/opentrace/onboarding/OTPFragment.kt
+++ app/src/main/java/io/bluetrace/opentrace/onboarding/OTPFragment.kt
@@ -81,7 +81,7 @@ class OTPFragment : OnboardingFragmentInterface() {
         sent_to.text = HtmlCompat.fromHtml(
             getString(
                 R.string.otp_sent,
-                "<b>${Preference.getPhoneNumber(context!!)}</b>"
+                "<b>${Preference.getPhoneNumber(requireContext())}</b>"
             ), HtmlCompat.FROM_HTML_MODE_LEGACY
         )
 
